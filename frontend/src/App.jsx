25ba7bcce00d2dc9a34f9082a0860b861f4fec46
sanchitpasricha <sanchit0229@gmail.com>
2024-06-28 03:03:52 +0530

    Setting up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { Send } from "./pages/Send";
 
 function App() {
-  return <div>Hellow World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Send" element={<Send />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
